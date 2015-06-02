62bbaa209168fcce8c1a5b02b6a5a47693c28fcf
Chris Brandsma <chris.brandsma@gmail.com>
2015-06-01 22:25:07 -0600

    refactored some stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'React Demos' });
+});
+
+router.get("/demo1", function(req, res, next){
+	res.render('demo1')
 });
 
 module.exports = router;
